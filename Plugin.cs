17da29567f4561c4b413a9219c869ac749a5f86c
John Gilbert <johngilb@gmail.com>
2026-09-23 22:53:30 -0700

    upversion (late)

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ namespace SailwindVirtualCrew
     {
         public const string PLUGIN_ID = "com.zorkinian.virtualcrew";
         public const string PLUGIN_NAME = "VirtualCrew";
-        public const string PLUGIN_VERSION = "0.2.1";
+        public const string PLUGIN_VERSION = "0.2.2";
 
         //--settings--
         internal static ConfigEntry<bool> exampleSetting;
